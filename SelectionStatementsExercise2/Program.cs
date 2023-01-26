27bfe3818f557e61f3dcf57e08affc10baaa9e6e
tiagogomesgh <tiagogomesfl@outlook.com>
2023-01-26 14:15:27 -0800

    Completed Excercise.

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace SelectionStatementsExercise2
+﻿using System.Diagnostics;
+
+namespace SelectionStatementsExercise2
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, What was your favorite subject in High School?");
+            var favSubject = Console.ReadLine();
+            switch(favSubject)
+            {
+                case "History":
+                    //  favSubject == "History";
+                    Console.WriteLine("History! Welcome!!!");
+                    break;
+                case "Math":
+                    //  favSubject == "History";
+                    Console.WriteLine("Math! Welcome!!!");
+                    break;
+                case "Science":
+                    //  favSubject == "History";
+                    Console.WriteLine("Science! Welcome!!!");
+                    break;
+                case "Lunch":
+                    //  favSubject == "History";
+                    Console.WriteLine("Hahaha Lunch! Welcome!!!");
+                    break;
+                
+                default:
+                    Console.WriteLine("Hmmmm... What can I do for you?");
+                    break;
+            }
         }
+
+
+       
     }
+
+
 }
